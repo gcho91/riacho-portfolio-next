349a4648f5b2ebeeb9fb7faebbf3f402dd99ee9e
gcho91@gmail.com <gcho91@gmail.com>
2023-07-31 16:51:19 -0700

    Update main page and structure, remove tailwind

---
+++ pages/home.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Home() {
+  return <div className="intro">
+    <h1 className="">Hello, my name is Ria.</h1>
+    <p className="subtext">I am a front-end engineer, vocalist, and singer-songwriter currently based in San Francisco.</p>
+    <button>Contact Me</button>
+    <style global jsx>{`
+      html,
+      body,
+      body > div:first-child,
+      div#__next,
+      div#__next > div {
+        height: 100%;
+      }
+    `}</style>
+  </div>
+
+
+
+
+}
