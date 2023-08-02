6e295baa255ba98fefd6069a17de5037c108c5d1
gcho91@gmail.com <gcho91@gmail.com>
2023-08-02 14:43:46 -0700

    Import font, add max-width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter, IBM_Plex_Sans } from 'next/font/google'
+
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: '400',
+  subsets: ['latin']
+})
+
+console.log(ibmPlexSans)
+// https://blog.logrocket.com/next-js-font-optimization-custom-google-fonts/#adding-google-fonts-next-font
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={ibmPlexSans.className}>
+    <Component {...pageProps} />
+  </main>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,49 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-// import image from "/portrait.png"
 
-const inter = Inter({ subsets: ['latin'] })
+import { IBM_Plex_Sans_Condensed } from 'next/font/google'
+
+const ibmPlexSansCond = IBM_Plex_Sans_Condensed({
+  weight: '600',
+  subsets: ['latin']
+})
+// todo: make font utility file
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className='home-container'>
       <main className='hero'>
+        <header className="header-section">
+          <div className='container-head'>
+            <div>
+              <span>Ria cho</span>
+            </div>
+            <nav>
+              <ul className='navbar-list'>
+                <li>
+                  <a href="index.html" className="nav-links">Home</a>
+                </li>
+                <li>
+                  <a href="about.html" className="nav-links">About</a>
+                </li>
+                <li>
+                  <a href="work.html" className="nav-links">Work</a>
+                </li>
+                <li>
+                  <a href="./img/Gloria-Cho_August-2019-vC1.pdf" className="nav-links">Resume</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+
+        </header>
+
         <div className='left'>
-          <h1 style={{ margin: 0 }}>Hello, my name is <br />
+          <h1 className={ibmPlexSansCond.className} style={{ margin: 0 }}>Hello, my name is <br />
             Ria Cho. </h1>
           <h2>I build things for the web and mobile</h2>
-          <p>I am a front-end engineer who specialies in building products with high end-user empathy and accessibility. Currently, I am working on x, y, z. </p>
-          <button>Contact me</button>
+          {/* <p>I am a front-end engineer who specialies in building products with high end-user empathy and accessibility. Currently, I am working on x, y, z. </p> */}
+          <p>I am a front-end engineer with obsession for end-user empathy, and passion for learning accessibility. Currently, I am working on xyz.</p>
+          <a className="cta-button">Contact me</a>
         </div>
       </main>
 
