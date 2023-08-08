8e2715f49ff3a7cfb9283951afc84afe92ca0623
gcho91@gmail.com <gcho91@gmail.com>
2023-08-07 18:11:33 -0700

    Added icons and footer

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,6 +3,12 @@ import menu from "../public/menu.png";
 import Image from 'next/image'
 import { useState } from "react";
 import heroStyles from "../styles/heroSectionStyles.module.css"
+import twitterIcon from "../public/twitter-icon.svg";
+import linkedinIcon from "../public/linkedin-icon.svg";
+import githubIcon from "../public/github-icon.svg";
+
+import TwitterIcon from "./TwitterIcon";
+
 
 export default function HeroSection() {
 
@@ -54,6 +60,28 @@ export default function HeroSection() {
             <h2>I build things for the <span className="intro-highlight">web</span> and <span className="intro-highlight">	mobile </span></h2>
             <p>I am a front-end engineer who loves building products with great UX, and is obsessed with high end-user empathy and usability. I am currently based in San Francisco.</p>
             <a href="mailto:gcho91@gmail.com" className={heroStyles['cta-button']}>Contact me</a>
+
+
+
+        </div>
+        {/* <h1>footer?</h1> */}
+        <div className={heroStyles["social-row"]}>
+            <a href="https://www.twitter.com/espresscho">
+
+                <Image priority src={twitterIcon} alt="Follow me on twitter"
+                    height={15}
+                    width={15} />
+            </a>
+            <a href="https://www.linkedin.com/in/gloriacho1">
+                <Image priority src={linkedinIcon} alt="Follow me on LinkedIn"
+                    height={15}
+                    width={15} />
+            </a>
+            <a href="https://www.github.com/gcho91">
+                <Image priority src={githubIcon} alt="Follow me on Github"
+                    height={15}
+                    width={15} />
+            </a>
         </div>
     </main>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,14 @@ import AboutMe from '@/components/AboutMe'
 
 export default function Home() {
   return (
-    <div className='home-container'>
-      <HeroSection />
-    </div >
+    <>
+      <div className='home-container'>
+        <HeroSection />
+      </div >
+      {/* <div>
+        <h1>hello world</h1>
+      </div> */}
+    </>
 
   )
 }
