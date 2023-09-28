23fe8783f925e038e99b775e91f47651ebcef9bc
gcho91@gmail.com <gcho91@gmail.com>
2023-09-28 13:24:03 -0700

    Update style for portfolio section

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,50 +1,62 @@
-import Image from 'next/image'
-import twitterIcon from "../public/twitter-icon.svg";
+import Image, { StaticImageData } from 'next/image'
 import fermatMock from "../public/fermat-mock.png"
+import amaltheaMock from "../public/amalthea-mock.png"
+import workStyles from "../styles/workStyles.module.css"
+import gmarketMock from "../public/gmarket-mock.png"
+import lynnsMock from "../public/lynns-mock.png"
 
+interface PortfolioItem {
+    imageUrl: StaticImageData
+    title: string;
+    desc: string;
+}
+
+const portfolioItems: PortfolioItem[] = [
+    {
+        imageUrl: fermatMock,
+        title: "Fermat Mock",
+        desc: "Some description text"
+    },
+    {
+        imageUrl: amaltheaMock,
+        title: "Amalthea Mock",
+        desc: "Amalthea coffee website"
+    },
+    {
+        imageUrl: gmarketMock,
+        title: "Ecom app",
+        desc: "Mock ecomm app"
+    },
+    {
+        imageUrl: lynnsMock,
+        title: "Lynn's NYC Supper Club",
+        desc: "Supper club wordpress site"
+    }
+]
 export default function Portfolio() {
-    return <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+    console.log(fermatMock, 'mock')
+    return <div className={workStyles.portfolioContainer}>
 
-
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
-        <div>
-            <Image style={{ maxWidth: "100%", height: "100%" }} src={fermatMock} alt="fermat mock" />
-            <p>text</p>
-        </div>
+        {portfolioItems.map(item => {
+            return <div key={item.desc} className={workStyles.item} style={{ backgroundImage: `url(${item.imageUrl.src})` }}>
+                <div className={workStyles.textWrapper}>
+                    <h2 className={workStyles.title}>{item.title}</h2>
+                    <p className={workStyles.desc}>{item.desc}</p>
+                </div>
+            </div>
+        })}
 
 
 
-        {/* <div>
-            <h1>Portfolio item 1</h1>
-            <Image src={fermatMock} alt="fermat mock" />
-        </div> */}
+
+
+
+
+
+
+
+
+
 
     </div>
 }
