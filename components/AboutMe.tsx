c914b1bde5df6d75d4674d1a7b6d4236d3b343e3
gcho91 <gcho91@gmail.com>
2024-01-12 14:21:14 -0600

    Update with draft aboutme section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,37 @@
+import aboutMeStyles from "../styles/aboutMeStyles.module.css"
+
 export default function AboutMe() {
     return <section>
-        <h2>about me</h2>
+        <h2 className={aboutMeStyles.label}>About me</h2>
+        <div className={aboutMeStyles.container}>
+            <div className="left">
+                <p>Hello, my name is Ria, and I&#39;m a front-end engineer currently splitting my time between San Francisco, Dallas, and New York. I enjoy using my experience as a former marketer and practicing my user-empathy to build enjoyable user experiences that convert to measurable results.</p>
+
+                <p>My last few roles have been focused on working with planning and implementation of design systems and customizing component libraries. I enjoy learning from others, and often find myself in mentoring roles. I am also a former conference speaker at UT Dallas DevFest by Google Developers Group.</p>
+
+                <p>Some technologies Ive been working with:</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>React-Native</li>
+                    <li>TypeScript</li>
+                    <li>Design Systems</li>
+                    <li>MUI</li>
+                </ul>
+
+                <p>Currently excited about:</p>
+                <ul>
+                    <li>Mobile Native Development (Swift, Objective-C)</li>
+                    <li>UX Research & Design</li>
+                </ul>
+            </div>
+            <div className="right">
+                <h1>some photo</h1>
+            </div>
+
+        </div>
+
+
+
     </section>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '../components/HeroSection'
 import Portfolio from './work'
 import Contact from './contact'
+import AboutMe from '@/components/AboutMe'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <AboutMe />
       <Portfolio />
       <Contact />
     </>
