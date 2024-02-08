f945b394a84bd029c94bfea8fc730ee89d3d0146
gcho91 <gcho91@gmail.com>
2024-02-08 15:06:51 -0600

    Add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps) {
   return <Layout >
     <Head>
+      <title>Ria Cho - Front-End Software Engineer</title>
       <link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎤</text></svg>" />
     </Head>
     <Component {...pageProps} />
